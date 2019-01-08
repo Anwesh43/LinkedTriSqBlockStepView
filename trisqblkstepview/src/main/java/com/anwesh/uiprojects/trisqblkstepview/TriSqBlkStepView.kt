a5441383d47fc987b51c77fedcc28f6ec023e471
Anwesh43 <anweshthecool0@gmail.com>
2019-01-08 10:43:42 +0530

    adding constants and importing libraries

--- trisqblkstepview/src/main/java/com/anwesh/uiprojects/trisqblkstepview/TriSqBlkStepView.kt
+++ trisqblkstepview/src/main/java/com/anwesh/uiprojects/trisqblkstepview/TriSqBlkStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.trisqblkstepview
 /**
  * Created by anweshmishra on 08/01/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val blocks : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.7f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
