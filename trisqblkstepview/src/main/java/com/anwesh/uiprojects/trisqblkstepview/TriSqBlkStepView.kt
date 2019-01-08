6a5beca6c9ab5d2844617b0e69e3af8279bbaeca
Anwesh43 <anweshthecool0@gmail.com>
2019-01-08 11:29:39 +0530

    adding TriSqBlkStepView class

--- trisqblkstepview/src/main/java/com/anwesh/uiprojects/trisqblkstepview/TriSqBlkStepView.kt
+++ trisqblkstepview/src/main/java/com/anwesh/uiprojects/trisqblkstepview/TriSqBlkStepView.kt
@@ -156,7 +156,30 @@ class TriSqBlkStepView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class TriSqBlkStep(var i : Int) {
+        private val root : TSBNode = TSBNode(0)
+        private var curr : TSBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
