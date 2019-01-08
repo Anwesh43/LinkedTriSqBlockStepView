443947ea8b993ec19a64a27a42c629fbf36d3a1c
Anwesh43 <anweshthecool0@gmail.com>
2019-01-08 11:27:11 +0530

    adding TSBNode class

--- trisqblkstepview/src/main/java/com/anwesh/uiprojects/trisqblkstepview/TriSqBlkStepView.kt
+++ trisqblkstepview/src/main/java/com/anwesh/uiprojects/trisqblkstepview/TriSqBlkStepView.kt
@@ -115,4 +115,48 @@ class TriSqBlkStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TSBNode(var i : Int, val state : State = State()) {
+
+        private var next : TSBNode ?= null
+        private var prev : TSBNode ?= null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTSBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TSBNode {
+            var curr : TSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
